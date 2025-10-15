943d380507e442a1fb5ed8e100e5522eb19501c9
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2025-10-15 20:45:42 +0530

    Added the server.js with routes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "../src/routes/userRoutes.js"
+import eventRoutes from "../src/routes/eventRoutes.js"
 
 const app = express();
 
@@ -10,7 +12,19 @@ app.get("/", (req, res) => {
   res.send("Server is working");
 });
 
-const PORT = 4000;
+app.use('/api/users', userRoutes);
+app.use('/api/events', eventRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
